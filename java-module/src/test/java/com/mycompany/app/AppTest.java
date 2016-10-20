5995b3e1d4d4a38e16741c9dfdf3bcc92d185eac
Alexey Udalykh <alexey.udaiykh@gteamstaff.com>
2016-10-20 21:29:23 +0800

    added more tests

--- java-module/src/test/java/com/mycompany/app/AppTest.java
+++ java-module/src/test/java/com/mycompany/app/AppTest.java
@@ -6,13 +6,25 @@ import static org.junit.Assert.assertTrue;
 
 public class AppTest {
 
+
     @Test
     public void shouldPass() {
         assertTrue(true);
     }
 
     @Test
+    public void shouldPassToo() {
+        assertTrue(true);
+    }
+
+
+    @Test
     public void shouldFail() {
+        assertTrue(false);
+    }
+
+    @Test
+    public void shouldFailToo() {
         assertTrue(false);
     }
 }
